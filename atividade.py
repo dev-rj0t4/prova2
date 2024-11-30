d3b5c92eee68866fb556113319126dec9d1c1dbf
dev-rj0t4 <renedesouza.jr@gmail.com>
2024-11-29 22:00:28 -0300

    first commit

---
+++ atividade.py
@@ -0,0 +1,14 @@
+dados = input("Digite seu nome")
+print ('Seja bem vindo ' + dados)
+
+#variavel do tipo string
+nome = 'rene'
+
+#variavel do tipo int
+idade = 25
+
+#variavel do tipo float
+numero = 2,23
+
+#variavel do tipo bool
+brasileiro = True
